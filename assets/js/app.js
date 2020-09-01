3a622dacde17cd0d422bba32b49a5d4b8bed1f66
Adel Sadek <adelabodoma@gmail.com>
2020-09-01 15:25:22 +0200

    add names pages

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,1 +1,21 @@
 new WOW().init();
+const slideContainer = document.getElementById('slides');
+
+$('#slider').slick({
+    infinite: false,
+    vertical:true,
+    verticalSwiping:true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    prevArrow: $('.top-arrow'),
+    nextArrow: $('.bottom-arrow'),
+    dots: true
+  });
+
+  var dotNums = document.querySelectorAll(".slick-dots button");
+
+  function removeText(item) {
+    item.innerHTML = ""; // or put the text you need inside quotes
+  }
+
+  dotNums.forEach(removeText);
