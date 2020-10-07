e8a1293054223b7468948cbea2531b2312f1c6e8
Mo'men <momen.hassan@kickstartinteractive.net>
2020-10-08 00:33:27 +0200

    animals-1: new changes

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,20 +2,30 @@ new WOW().init();
 const slideContainer = document.getElementById('slides');
 
 $('#slider').slick({
-    infinite: false,
-    vertical:true,
-    verticalSwiping:true,
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    prevArrow: $('.top-arrow'),
-    nextArrow: $('.bottom-arrow'),
-    dots: true
-  });
+  infinite: false,
+  vertical: true,
+  verticalSwiping: true,
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  prevArrow: $('.top-arrow'),
+  nextArrow: $('.bottom-arrow'),
+  dots: true
+});
 
-  var dotNums = document.querySelectorAll(".slick-dots button");
+$('#slider-2').slick({
+  infinite: false,
+  adaptiveHeight:true,
+  rtl:true,
+  slidesToShow: 4,
+  slidesToScroll: 4,
+  prevArrow:false,
+  nextArrow: false,
+  dots: true
+});
+var dotNums = document.querySelectorAll(".slick-dots button");
 
-  function removeText(item) {
-    item.innerHTML = ""; // or put the text you need inside quotes
-  }
+function removeText(item) {
+  item.innerHTML = ""; // or put the text you need inside quotes
+}
 
-  dotNums.forEach(removeText);
+dotNums.forEach(removeText);
